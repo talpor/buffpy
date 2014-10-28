545f8133da3dddf31d4ac6d89ae9cae78f374fc7
Alberto Sanchez <asanchez@talpor.com>
2014-10-28 13:39:18 -0430

    some pep8

--- buffpy/exceptions.py
+++ buffpy/exceptions.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class BuffpyException(Exception):
     pass
 
@@ -13,7 +14,8 @@ class BuffpyRestException(BuffpyException):
 
     """
 
-    def __init__(self, url, http_code, error_code=None, description=None, method='GET'):
+    def __init__(self, url, http_code, error_code=None, description=None,
+                 method='GET'):
         self.url = url
         self.http_code = http_code
         self.error_code = error_code
@@ -29,12 +31,15 @@ class BuffpyRestException(BuffpyException):
         if hasattr(sys.stderr, 'isatty') and sys.stderr.isatty():
             msg = (
                 "\nHTTP Error - Your request was:\n\n{request}"
-                "\n\nBuffer returned the following error message:\n\nHTTP {http_code} error: {error_code} description: {description}\n".format(
+                "\n\nBuffer returned the following error message:"
+                "\n\nHTTP {http_code} error: {error_code} description: "
+                "{description}\n".format(
                     request="%s %s" % (self.method, self.url),
-                    http_code = self.http_code,
-                    error_code = self.error_code,
+                    http_code=self.http_code,
+                    error_code=self.error_code,
                     description=self.description
                 ))
             return msg
         else:
-            return "HTTP {} error: {} description: {}".format(self.http_code, self.error_code, self.description)
+            return "HTTP {} error: {} description: {}".format(
+                self.http_code, self.error_code, self.description)
